c11ebaa7dfc2d8d3135a0895d9c1564a42a3d30f
Tomasz Libich <tomaszlibich@tomaszs-mbp.osb.ft.com>
2016-09-16 14:13:02 +0100

    Removing Procfile

--- server-start.js
+++ server-start.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3000;
 
 require('pushstate-server').start({
     port: PORT,
-    directory: './build'
+    directory: './'
 });
 
 console.info('[boot] Running server on port ' + PORT + '...');
