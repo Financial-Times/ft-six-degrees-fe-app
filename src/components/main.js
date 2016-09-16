850f399b2bb6a14186b7dc0fa7ec1ff07bd6f01e
Tomasz Libich <tomaszlibich@tomaszs-mbp.osb.ft.com>
2016-09-16 15:28:30 +0100

    Testing build on Heroku

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,7 @@ class Main extends Component {
   render() {
     return (
       <div className="main">
-        <h2>FT Six Degrees</h2>
+        <h2>FT Six Degrees FE App</h2>
       </div>
     );
   }
