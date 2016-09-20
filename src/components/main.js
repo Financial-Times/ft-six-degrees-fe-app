9b59c2c11e96ed7afeec63f5beee7fa54deabd62
ammar.hassan <ammar.hassan@ft.com>
2016-09-20 11:29:48 +0100

    adding extra text to app desc

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,7 @@ class Main extends Component {
   render() {
     return (
       <div className="main">
-        <h2>FT Six Degrees FE App</h2>
+        <h2>FT Six Degrees FE App Ammar test another</h2>
       </div>
     );
   }
