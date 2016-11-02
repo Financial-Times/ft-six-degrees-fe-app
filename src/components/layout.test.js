cf3c75b9219db93b181004e57a7a95e868a36bd9
Tomasz Libich <tomasz.libich@ft.com>
2016-11-02 10:21:09 +0000

    Updating layout test to reflect latest changes

--- src/components/layout.test.js
+++ src/components/layout.test.js
@@ -3,6 +3,15 @@ import {render} from 'react-dom';
 import Layout from './layout';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<Layout />, div);
+  const div = document.createElement('div'),
+    children = React.createElement('div'),
+    noop = () => {
+      return;
+    },
+    store = {
+        dispatch: noop,
+        getState: noop,
+        subscribe: noop
+    };
+  render(<Layout store={store} children={children} />, div);
 });
