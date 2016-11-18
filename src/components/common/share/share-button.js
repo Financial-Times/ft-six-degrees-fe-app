ca5e21f4ee69d9a45e3fc02bffccfe19b8a9e877
Tomasz Libich <tomasz.libich@ft.com>
2016-11-18 13:43:24 +0000

    Adding simple versions of Twitter and Facebook sharers

--- src/components/common/share/share-button.js
+++ src/components/common/share/share-button.js
@@ -6,14 +6,15 @@ class ShareButton extends React.Component {
         const iconClassName = 'share-button fa fa-' + this.props.icon;
 
         return (
-            <a href={this.props.href} className={iconClassName} target="_blank"></a>
+            <a href={this.props.href} className={iconClassName} target="_blank">{this.props.label}</a>
         );
     }
 };
 
 ShareButton.propTypes = {
     href: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
+    icon: PropTypes.string.isRequired,
+    label: PropTypes.string
 };
 
 export default ShareButton;
--- src/components/common/share/share.js
+++ src/components/common/share/share.js
@@ -6,8 +6,8 @@ const Share = () => {
     return (
         <div className="share-container">
             <span className="share-label">Share</span>
-            <ShareButton icon="twitter" href="http://www.ft.com" />
-            <ShareButton icon="facebook" href="http://www.ft.com" />
+            <ShareButton icon="twitter" href="https://twitter.com/intent/tweet?text=Use Six Degree's to figure out who has made the headlines, discover their associations and unearth the stories that connect them&url=https://six-degrees.ft.com/" />
+            <ShareButton icon="facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://six-degrees.ft.com/" />
         </div>
     );
 };
