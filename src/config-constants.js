518458465518cc7145e6835eb987b87f23248edf
Tomasz Libich <tomasz.libich@ft.com>
2016-11-21 10:08:40 +0000

    Adding a temporary app URL handler to determine NodeJS proxy API url

--- src/config-constants.js
+++ src/config-constants.js
@@ -1,8 +1,19 @@
 const storedSixDegreesApiUrl = window.localStorage && window.localStorage.getItem('six-degrees-api') ? window.localStorage.getItem('six-degrees-api') : null;
 
+function isDevOrTest() {
+    const href = window.location.href;
+    let devTestUrl = null;
+    if (href.indexOf('six-degrees-dev.ft.com')!== -1) {
+        devTestUrl = 'https://ft-six-degrees-be-app-develop.herokuapp.com/';
+    } else if (href.indexOf('six-degrees-test.ft.com')!== -1) {
+        devTestUrl = 'https://ft-six-degrees-be-app-staging.herokuapp.com/'
+    }
+    return devTestUrl;
+}
+
 export const CONFIG = {
     URL: {
-        API: storedSixDegreesApiUrl || 'https://ft-six-degrees-be-app.herokuapp.com/',
+        API: storedSixDegreesApiUrl || isDevOrTest() || 'https://ft-six-degrees-be-app.herokuapp.com/',
     },
     TEXT: {
         DATE_RANGE: {
