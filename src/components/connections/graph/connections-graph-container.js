5c4e8eb0d511b27641ec2e528030de8fded4ae74
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-07-26 10:41:13 +0300

    Update the graph container

--- src/components/connections/graph/connections-graph-container.js
+++ src/components/connections/graph/connections-graph-container.js
@@ -1,91 +1,67 @@
-import React, {PropTypes} from 'react';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import {CONFIG} from '../../../config-constants';
-import * as connectedPeopleChainActions from '../../../actions/connected-people-chain-actions';
-import * as connectionsRootActions from '../../../actions/connections-root-actions';
+import React, { PropTypes } from 'react';
+import UuidUtils from '../../../services/uuid.utils';
+import { connect } from 'react-redux';
+import * as connectionsActions from '../../../actions/dd-connections-actions';
 import * as hintActions from '../../../actions/hint-actions';
+import { setRootConnection } from '../../../actions/dd-root-connection-actions';
+import { setActiveRootConnection } from '../../../actions/dd-active-root-connection-actions';
 import Graph from 'react-graph-vis';
 import './connections-graph-container.css';
+import { getGraphNodes, getGraphEdges } from '../../../selectors';
+import graphOptions from './graph-options';
 
 class ConnectionsGraphContainer extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.options = {
-			layout: {
-				hierarchical: false
-			},
-			autoResize: true,
-			physics: {
-				barnesHut: {
-					avoidOverlap: 0.8,
-					gravitationalConstant: -5000,
-					springConstant: 0.01,
-					centralGravity: 0
-				}
-			},
-			edges: {
-				color: "#000000"
+	graphEvents() {
+		const { loadConnections } = this.props;
+		return {
+			select: (event) => {
+				let { nodes, edges } = event;
+				console.log("Selected nodes:");
+				console.log(nodes);
+				console.log("Selected edges:");
+				console.log(edges);
+				let rootId = nodes[0];
+				console.log('rootId', rootId);
+				loadConnections(UuidUtils.extract(rootId));
 			}
 		};
 	}
 
-	updateHint(name) {
-		this.props.actions.hint.change(CONFIG.TEXT.HINT.SELECT_ASSOCIATION.replace('####', name));
+	loadData() {
+		let {
+			loadConnections,
+			setRootConnection,
+			routeParams
+		} = this.props;
+		loadConnections(routeParams.id);
+		setRootConnection(routeParams.id);
 	}
 
-	updateConnectionsRootPerson(uuid, peopleData) {
-		const person = peopleData.filter(person => {
-			return person.id.indexOf(uuid) !== -1;
-		})[0];
-
-		this.props.actions.connectionsRoot.change(person);
-		// this.props.actions.connectedPeopleChain.update([person]);
-		this.updateHint(person.abbrName);
+	componentDidMount() {
+		this.loadData();
 	}
 
-	componentDidUpdate() {
-		const peopleData = [].concat(this.props.personalisedPeopleData, this.props.mentionedPeopleData);
-
-		if (!this.props.connectionsRoot.id && peopleData.length) {
-			this.updateConnectionsRootPerson(this.props.router.params.id, peopleData);
-		} else {
-			this.updateHint(this.props.connectionsRoot.abbrName);
-		}
-	}
 
 	render() {
 		let style = {
 			width: '100%',
 			height: '600px'
 		};
-		if(!this.props.connectedPeopleChain.length) {
+
+		if(!Object.keys(this.props.connectedPeopleChain).length) {
 			return <div>Loadin...</div>;
 		}
-		let graph = {
-			nodes: [],
-			edges: []
+
+		const graph = {
+			nodes: this.props.graphNodes,
+			edges: this.props.graphEdges
 		};
-		let people = this.props.connectedPeopleChain.map(p => p.person);
-		let nodes = people.map(p => ({
-			id: p.id,
-			label: p.prefLabel,
-			size: 20,
-			shape: "circularImage",
-			image: p.img || 'https://randomuser.me/api/portraits/lego/7.jpg'
-		}));
-		graph.edges = nodes.map(p => ({
-			from: this.props.connectionsRoot.id,
-			to: p.id,
-			length: 80
-		}));
-		nodes.push({id: this.props.connectionsRoot.id, label: this.props.prefLabel, size: 30, shape: 'circularImage', image: this.props.connectionsRoot.img});
-		graph.nodes = [...nodes];
+
 		return (
 			<div className="connections-graph-container">
 				<div id="connections-graph" className="connections-graph">
-					<Graph style={style} graph={graph} options={this.options} events={this.events} />
+					<Graph style={style} graph={graph} options={graphOptions} events={this.graphEvents()} />
 				</div>
 			</div>
 		);
@@ -93,28 +69,34 @@ class ConnectionsGraphContainer extends React.Component {
 }
 
 ConnectionsGraphContainer.propTypes = {
-	connectedPeopleChain: PropTypes.array.isRequired,
+	connectedPeopleChain: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+	personalisedPeopleData: PropTypes.array.isRequired,
 	connectionsRoot: PropTypes.object.isRequired,
 	router: React.PropTypes.object.isRequired
 };
 
-function mapStateToProps(state, ownProps) {
-	return {
+const makeMapStateToProps = (state, ownProps) => {
+	const graphNodes = getGraphNodes();
+	const graphEdges = getGraphEdges();
+	const mapStateToProps = (state, ownProps) => ({
+		graphNodes: graphNodes(state, ownProps),
+		graphEdges: graphEdges(state, ownProps),
 		connectedPeopleChain: state.connectedPeopleChain,
 		connectionsRoot: state.connectionsRoot,
 		mentionedPeopleData: state.mentionedPeopleData,
+		activeRootConnection: state.activeRootConnection,
+		loginState: state.loginState,
 		personalisedPeopleData: state.personalisedPeopleData
-	};
-}
+	});
+	return mapStateToProps;
+};
 
-function mapDispatchToProps(dispatch) {
-	return {
-		actions: {
-			connectedPeopleChain: bindActionCreators(connectedPeopleChainActions, dispatch),
-			connectionsRoot: bindActionCreators(connectionsRootActions, dispatch),
-			hint: bindActionCreators(hintActions, dispatch)
-		}
-	};
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ConnectionsGraphContainer);
+export default connect(
+	makeMapStateToProps,
+	{
+		loadConnections: connectionsActions.loadConnections,
+		setActiveRootConnection,
+		setRootConnection,
+		hint: hintActions
+	}
+)(ConnectionsGraphContainer);
