385ddb65c5839614c63fe22c98377a18232dd8b5
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-07-26 10:45:45 +0300

    Update middleware stack

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,17 +2,20 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+// import apiMiddleware from '../middlewares/api';
+import { apiMiddleware } from 'redux-api-middleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let middlewares = [thunk, apiMiddleware];
 
-const middleware = process.env.NODE_ENV !== 'production' ?
-	[reduxImmutableStateInvariant(), thunk] :
-	[thunk];
+middlewares = process.env.NODE_ENV !== 'production' ?
+	[...middlewares, reduxImmutableStateInvariant()] :
+	[...middlewares];
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(...middleware))
+        composeEnhancers(applyMiddleware(...middlewares))
     );
 }
