e8e96fcac5171d706a48535c8d284b8755781d8c
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-07-26 10:43:40 +0300

    Update connections root reducer

--- src/reducers/connections-root-reducer.js
+++ src/reducers/connections-root-reducer.js
@@ -1,13 +1,12 @@
-import initialState from '../store/initial-state';
 import * as types from '../actions/action-types';
 
-export default function connectionsRootReducer(state = initialState.connectionsRoot, action) {
-
-    switch (action.type) {
-        case types.CONNECTIONS_ROOT_CHANGE:
-        return action.connectionsRoot;
-    default:
-        return state;
-    }
-
+export default function connectionsRootReducer(state = {}, action) {
+	switch (action.type) {
+		case types.SET_ACTIVE_ROOT_CONNECTION:
+			return action.connection;
+		case types.CONNECTIONS_ROOT_CHANGE:
+			return action.connectionsRoot;
+		default:
+			return state;
+	}
 }
