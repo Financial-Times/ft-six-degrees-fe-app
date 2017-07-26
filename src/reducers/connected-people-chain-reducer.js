885c4a40ce9564a82f37a03aeeb6feaba401c1c0
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-07-26 10:43:13 +0300

    Update connections reducer

--- src/reducers/connected-people-chain-reducer.js
+++ src/reducers/connected-people-chain-reducer.js
@@ -1,13 +1,20 @@
 import initialState from '../store/initial-state';
 import * as types from '../actions/action-types';
+import isEmpty from 'lodash/isEmpty';
 
 export default function connectedPeopleChainReducer(state = initialState.connectedPeopleChain, action) {
 
     switch (action.type) {
+	    case types.RESET_CONNECTIONS:
+	    	return {};
         case types.CONNECTED_PEOPLE_CHAIN_UPDATE:
             return action.connectedPeopleChain;
+	    case types.CONNECTIONS_SUCCESS:
+	    	return {
+			    ...state,
+			    [action.meta]: isEmpty(action.payload) ? [] : action.payload
+		    };
 		default:
 			return state;
     }
-
 }
