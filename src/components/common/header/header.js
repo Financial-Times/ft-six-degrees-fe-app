b90e04cc3fe6a29be4e64d5b7c5f031b67c56b72
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-08-16 11:30:10 +0300

    Update header component

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -22,10 +22,15 @@ class Header extends React.Component {
                     <div className="o-header__row o-header__top">
                         <div className="o-header__container">
                             <div className="o-header__top-wrapper">
+                                <div className="o-header__top-column o-header__top-column--left">
+                                </div>
                                 <div className="o-header__top-column o-header__top-column--center">
                                     <a className="o-header__top-logo" href="/" title="Go to Financial Times homepage">
                                         <span className="o-header__visually-hidden">Financial Times</span>
                                     </a>
+                                </div>
+                                <div className="o-header__top-column o-header__top-column--right">
+									<i className="o-icons-icon o-icons-icon--share six-deg-logo"></i>
                                 </div>
                             </div>
                         </div>
