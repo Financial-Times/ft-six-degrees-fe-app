98a5d847afd97bbe27daece0355ee5661c697eae
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-08-16 11:47:00 +0300

    Update connections graph selectors

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,16 +1,20 @@
 import { createSelector } from 'reselect';
+import { CONFIG } from '../config-constants';
 import UuidUtils from '../services/uuid.utils';
+import isEmpty from 'lodash/isEmpty';
 
 const getConnections = (state, props) => state.connectedPeopleChain;
 const getRootConnection = (state, props) => state.rootConnection;
+const getActiveRootConnection = (state, props) => state.activeRootConnection;
 
+//connections graph
 const getNodeProps = (node) => {
 	return {
 		id: UuidUtils.extract(node.id),
 		label: node.abbrName,
 		size: node.isRoot ? 40 : 20,
 		shape: "circularImage",
-		image: node.img || 'https://randomuser.me/api/portraits/lego/7.jpg'
+		image: node.img || CONFIG.PLACEHOLDER_IMG
 	};
 };
 export const getGraphNodes = () => {
@@ -29,7 +33,7 @@ export const getGraphNodes = () => {
 					.map(getNodeProps);
 				graphNodes.push(...conns, getNodeProps(rootConnection));
 			});
-			return graphNodes;
+			return [...graphNodes];
 		}
 	);
 };
@@ -50,8 +54,35 @@ export const getGraphEdges = () => {
 				let edges = connections[rootId].map(c => getEdges(c.person, rootId));
 				graphEdges.push(...edges);
 			});
-			return graphEdges;
+			return [...graphEdges];
 		}
 	);
 };
 
+//connections/subheader
+export const getDirectConnectionsNumberForActiveRoot = () => {
+	return createSelector(
+		[ getConnections, getActiveRootConnection ],
+		(connections, activeRootConnection) => {
+			if ( !isEmpty(activeRootConnection) && !isEmpty(connections)) {
+				let activeRootId = UuidUtils.extract(activeRootConnection.person.id);
+				if (connections.hasOwnProperty(activeRootId)) {
+					return connections[activeRootId].length;
+				}
+			}
+		}
+	);
+};
+
+//dd-related-content
+export const getRelatedContent = () => {
+	return createSelector(
+		[ getActiveRootConnection ],
+		(activeRootConnection) => {
+			console.log('activeRootConnection', activeRootConnection);
+			if (!isEmpty(activeRootConnection)) {
+				return activeRootConnection.content;
+			}
+		}
+	);
+};
