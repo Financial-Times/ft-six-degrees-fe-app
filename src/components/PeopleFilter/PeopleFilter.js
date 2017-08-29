0a3b8443401079dfb566746b0d9f9c44308d3527
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-08-29 11:41:01 +0300

    Update people filter component

--- src/components/PeopleFilter/PeopleFilter.js
+++ src/components/PeopleFilter/PeopleFilter.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FtButton } from '../../components';
 import { PEOPLE_SELECTOR, PEOPLE_DATE_RANGE } from '../../config';
 import './PeopleFilter.css';
 
-const PeopleFilterContainer = ({
+const PeopleFilter = ({
 	dateRange,
 	peopleSelector,
 	peopleSelectorChange,
@@ -14,59 +15,81 @@ const PeopleFilterContainer = ({
 		peopleSelectorChange(val);
 	};
 	return (
-		<div className="people-filter-wrapper">
-			<form className="people-filter-form">
-				<fieldset>
-					<legend>FILTER</legend>
-					<div
-						className="people-filter-section"
-						data-o-grid-colspan="6"
-					>
-						<div className="people-filter-label">
-							Select people that appear
-						</div>
-						<div data-colspan="6" className="o-buttons__group">
-							{Object.keys(PEOPLE_SELECTOR).map(key => {
-								const label = PEOPLE_SELECTOR[key]['LABEL'];
-								const value = PEOPLE_SELECTOR[key]['VAL'];
-								return (
-									<FtButton
-										key={key}
-										selected={peopleSelector === value}
-										label={label}
-										className="o-buttons"
-										onClick={selectorClickHandler(value)}
-									/>
-								);
-							})}
-						</div>
-					</div>
-					<div
-						className="people-filter-section"
-						data-o-grid-colspan="6"
-					>
-						<div className="people-filter-label">from the last</div>
-						<div className="o-buttons__group">
-							{PEOPLE_DATE_RANGE.map(r => {
-								return (
-									<FtButton
-										key={r}
-										label={r}
-										selected={dateRange === r}
-										className="o-buttons"
-										onClick={e => {
-											e.preventDefault();
-											peopleDateRangeChange(r);
-										}}
-									/>
-								);
-							})}
-						</div>
-					</div>
-				</fieldset>
-			</form>
+		<div className="o-grid-row">
+			<div data-o-grid-colspan="12">
+				<div className="people-filter-wrapper">
+					<form className="people-filter-form">
+						<fieldset>
+							<legend>FILTER</legend>
+							<div
+								className="people-filter-section"
+								data-o-grid-colspan="6"
+							>
+								<div className="people-filter-label">
+									Select people that appear
+								</div>
+								<div
+									data-colspan="6"
+									className="o-buttons__group"
+								>
+									{Object.keys(PEOPLE_SELECTOR).map(key => {
+										const label =
+											PEOPLE_SELECTOR[key]['LABEL'];
+										const value =
+											PEOPLE_SELECTOR[key]['VAL'];
+										return (
+											<FtButton
+												key={key}
+												selected={
+													peopleSelector === value
+												}
+												label={label}
+												className="o-buttons"
+												onClick={selectorClickHandler(
+													value
+												)}
+											/>
+										);
+									})}
+								</div>
+							</div>
+							<div
+								className="people-filter-section"
+								data-o-grid-colspan="6"
+							>
+								<div className="people-filter-label">
+									from the last
+								</div>
+								<div className="o-buttons__group">
+									{PEOPLE_DATE_RANGE.map(r => {
+										return (
+											<FtButton
+												key={r}
+												label={r}
+												selected={dateRange === r}
+												className="o-buttons ft-button--quarter"
+												onClick={e => {
+													e.preventDefault();
+													peopleDateRangeChange(r);
+												}}
+											/>
+										);
+									})}
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default PeopleFilterContainer;
+PeopleFilter.PropTypes = {
+	dateRange: PropTypes.string.isRequired,
+	peopleSelector: PropTypes.string.isRequired,
+	peopleSelectorChange: PropTypes.func.isRequired,
+	peopleDateRangeChange: PropTypes.func.isRequired
+};
+
+export default PeopleFilter;
