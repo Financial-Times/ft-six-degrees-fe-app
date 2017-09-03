8db22712f19d75d553309dc14079e486bca90d62
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-09-03 15:17:52 +0300

    Update start over header icon to be responsive

--- src/components/Header/RestartIcon.js
+++ src/components/Header/RestartIcon.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
+import { breakpoints } from '../../config';
 
-const RestartIcon = () =>
+const { M } = breakpoints;
+
+const RestartIcon = () => (
 	<Link to="/people" className="six-deg-restart">
 		<i className="o-icons-icon o-icons-icon--refresh six-deg-icon" />
-		<span className="six-deg-icon-label">Start over</span>
-	</Link>;
+		<MediaQuery minDeviceWidth={M}>
+			<span className="six-deg-icon-label">Start over</span>
+		</MediaQuery>
+	</Link>
+);
 
 export default RestartIcon;
