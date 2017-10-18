cc60e0a73ca44e29beb7fc06be3bcdbce16d27b4
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-10-18 15:19:18 +0300

    Fix for article list component

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ArticleListItem from './ArticleListItem';
 import './ArticleList.css';
 
-const ArticleList = ({ articles }) =>
+const ArticleList = ({ articles }) => (
 	<div className="o-grid-row article-list">
-		{articles.map(a => <ArticleListItem key={a.id} article={a} />)}
-	</div>;
+		{articles.length &&
+			articles.map(a => <ArticleListItem key={a.id} article={a} />)}
+	</div>
+);
 
 export default ArticleList;
